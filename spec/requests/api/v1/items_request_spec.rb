82e40fa781dc9eff9a33ff3432aa9a7d011da9c3
William Homer <billyhomeriii@gmail.com>
2019-06-24 14:40:55 -0600

    add show method with tests to return single item

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -4,8 +4,25 @@ describe 'items api' do
   it 'sends a list of all items' do
     create_list(:item, 3)
 
-    get 'api/v1/items'
+    get '/api/v1/items'
 
-    expect response.to be_successful
+    expect(response).to be_successful
+
+    items = JSON.parse(response.body)
+
+    expect(items.count).to eq(3)
+  end
+
+  it 'can return a single item' do
+    id = create(:item).id
+
+    get "/api/v1/items/#{id}"
+
+    expect(response).to be_successful
+
+    item = JSON.parse(response.body)
+
+    expect(item["id"]).to eq(id)
+
   end
 end
